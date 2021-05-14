33ba790aa950c7494cecdea9731d22a88c3a0256
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2021-05-14 15:14:46 +0700

    Data Layer, Library OkHttp, Retrofit.

---
+++ app/src/main/java/com/example/cinema/data/api/ApiService.kt
@@ -0,0 +1,27 @@
+package com.example.cinema.data.api
+
+import com.example.cinema.data.response.ResponseMovie
+import com.example.cinema.data.response.ResponseMovieDetail
+import com.example.cinema.data.response.ResponseMovieReview
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface ApiService {
+
+    @GET("movie/upcoming?api_key=${ApiConfig.ApiKey}")
+    fun getMoviesUpComing() : Call<ResponseMovie>
+
+    @GET("movie/popular?api_key=${ApiConfig.ApiKey}")
+    fun getMoviesPopular() : Call<ResponseMovie>
+
+    @GET("movie/now_playing?api_key=${ApiConfig.ApiKey}")
+    fun getMoviesNowPlaying() : Call<ResponseMovie>
+
+    @GET("/movie/{movie_id}?api_key=${ApiConfig.ApiKey}")
+    fun getMovieDetail(@Path("movie_id") id: String): Call<ResponseMovieDetail>
+
+    @GET("/movie/{movie_id}/reviews?api_key=${ApiConfig.ApiKey}")
+    fun getMovieReview(@Path("movie_id") id: String): Call<ResponseMovieReview>
+}
--- app/src/main/java/com/example/cinema/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/cinema/ui/home/HomeViewModel.kt
@@ -3,6 +3,11 @@ package com.example.cinema.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cinema.data.api.ApiConfig
+import com.example.cinema.data.response.ResponseMovie
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +15,49 @@ class HomeViewModel : ViewModel() {
         value = "This is Home Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun getMoviesUpComing(){
+        val clients = ApiConfig.getApiService().getMoviesUpComing()
+        clients.enqueue(object : Callback<ResponseMovie> {
+            override fun onResponse(
+                call: Call<ResponseMovie>,
+                response: Response<ResponseMovie>
+            ) {
+                // Jika sukses.
+            }
+            override fun onFailure(call: Call<ResponseMovie>, t: Throwable) {
+                // Jika gagal.
+            }
+        })
+    }
+
+    fun getMoviesPopular(){
+        val clients = ApiConfig.getApiService().getMoviesPopular()
+        clients.enqueue(object : Callback<ResponseMovie> {
+            override fun onResponse(
+                call: Call<ResponseMovie>,
+                response: Response<ResponseMovie>
+            ) {
+                // Jika sukses.
+            }
+            override fun onFailure(call: Call<ResponseMovie>, t: Throwable) {
+                // Jika gagal.
+            }
+        })
+    }
+
+    fun getMoviesNowPlaying(){
+        val clients = ApiConfig.getApiService().getMoviesNowPlaying()
+        clients.enqueue(object : Callback<ResponseMovie> {
+            override fun onResponse(
+                call: Call<ResponseMovie>,
+                response: Response<ResponseMovie>
+            ) {
+                // Jika sukses.
+            }
+            override fun onFailure(call: Call<ResponseMovie>, t: Throwable) {
+                // Jika gagal.
+            }
+        })
+    }
 }
